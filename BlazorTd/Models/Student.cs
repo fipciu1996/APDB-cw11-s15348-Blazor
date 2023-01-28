247ac5dae21bc5973e0fd49ff6451ee51f1206c5
Mateusz Filipek <s15348@pjwstk.edu.pl>
2023-01-28 11:49:39 +0100

    Zadanie 11

---
+++ BlazorTd/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorTd.Models
+{
+    public class Student
+    {
+        public int IdStudent { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Studies { get; set; }
+        public string AvatarUrl { get; set; }
+    }
+}
